434bf85f2535b9888fbed46acf058b8137eb4cdd
Omolemo Tlomatsane <tycotecsa@gmail.com>
2024-11-24 16:40:34 +0200

    typescript

---
+++ utils.ts
@@ -0,0 +1,40 @@
+const reviewTotalDisplay = document.querySelector('#reviews')
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
+import { LoyaltyUser, Permissions } from './src/enums'
+import  Review  from './src/interfaces'
+
+export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) {
+    const iconDisplay = LoyaltyUser.GOLD_USER ? '⭐' : ''
+   
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+}
+
+export function populateUser(isReturning : boolean, userName: string ) {
+    if (isReturning){
+    
+        returningUserDisplay.innerHTML = 'back'
+    }
+   
+    userNameDisplay.innerHTML = userName
+}
+
+export function showDetails(value: boolean | Permissions, element : HTMLElement, price: number) {
+    if (value) {
+        const priceDisplay = document.createElement('div')
+        priceDisplay.innerHTML = price.toString() + '/night'
+        element.appendChild(priceDisplay)
+    }
+}
+
+
+export function makeMultiple(value: number) : string {
+    if (value > 1 || value == 0) {
+        return 's'
+    } else return ''
+}
+
+export function getTopTwoReviews(reviews : Review[]) : Review[]  {
+    const sortedReviews = reviews.sort((a, b) => b.stars - a.stars)
+    return sortedReviews.slice(0,2)
+   }
